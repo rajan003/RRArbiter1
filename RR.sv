42d481fce5495425b0b6e3e545cc7aa443380db1
Anurag Singh <anuragsi003@gmail.com>
2023-08-07 23:07:56 +0530

    Update RR.sv
    Rev-2

--- RR.sv
+++ RR.sv
@@ -11,20 +11,26 @@ module rr_arbiter #(
 );
 
   // Write your logic here...
-  logic [NUM_PORTS-1:0]m;
+  logic [$clog2(NUM_PORTS)-1:0]m;
+  logic [$clog2(NUM_PORTS)-1:0]p;
+
+  int n; ///
   always@(posedge clk, posedge reset)
     begin
       if(reset) m <= '0;
-      else m <= n;
+      else m <= p;  /////// Pointer to locate the bit which was granted access last time..
     end 
   
   always_comb
     begin 
-      for(int n=0; n<= NUM_PORTS; n++)
+        p=m;  ////Putting default to m 
+        gnt_o=4'b0; /// putting default to 0 
+      for(int n=1; n<= NUM_PORTS; n++)
         begin 
-          if(req_i[n]==1 && ( n==0 || n > m))
+          if(req_i[n-1]==1 && ( n >= m && m==p))  /////////taking care of 1 grant per clock cycle
             begin 
               gnt_o[n]=1'b1;
+              p=n;
             end
           else gnt_o[n] =1'b0;
         end

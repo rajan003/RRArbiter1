6d45440fd94aaaf5af708a4cc19d56b6da11b05d
Anurag Singh <anuragsi003@gmail.com>
2023-08-10 01:00:06 +0530

    Update RR.sv
    Final Code

--- RR.sv
+++ RR.sv
@@ -12,12 +12,11 @@ module rr_arbiter #(
 
   // Write your logic here...
   logic [NUM_PORTS-1:0]m;
+	logic [NUM_PORTS-1:0]req_i_masked;  //// Masked Request with last Served Input bit
  logic [NUM_PORTS-1:0]gnt_local;
  logic [NUM_PORTS-1:0]pos_r;
  logic [NUM_PORTS-1:0]pos;
-// logic [$clog2(NUM_PORTS)-1:0]m;
-//  logic [$clog2(NUM_PORTS)-1:0]p;
- // logic [NUM_PORTS-1:0]reg_in;
+
  always@(posedge clk, posedge reset)
     begin
       if(reset) begin 
@@ -33,10 +32,11 @@ module rr_arbiter #(
 always_comb
 begin 
 gnt_local=4'b0;
-if(req_i>m ) begin   /////////If the Requested Clients value is Greater than the Last Granted value//////
+if(req_i==m) gnt_local=m;
+else if(req_i_masked>m ) begin   /////////If the Requested Clients(masked) value is Greater than the Last Granted value//////
    for( int n=pos_r; n<= NUM_PORTS-1; n++)
 	begin 
-          if(req_i[n]==1)// && p==2'b00)
+	 if(req_i_masked[n]==1)
             begin 
               gnt_local[n]=1'b1;
             end
@@ -45,10 +45,10 @@ if(req_i>m ) begin   /////////If the Requested Clients value is Greater than the
                end
 	end
   end
-else if(req_i<=gnt_o ) begin  /////////If the Requested Clients value is less than or equal to  the Last Granted value//////
+  else if(req_i_masked<=gnt_o ) begin  /////////If the Requested Clients value is less than or equal to  the Last Granted value//////
    for( int n=0; n<= pos; n++)
 	begin 
-          if(req_i[n]==1)
+	if(req_i_masked[n]==1)
             begin 
               gnt_local[n-1]=1'b1;
             end
@@ -60,7 +60,8 @@ else if(req_i<=gnt_o ) begin  /////////If the Requested Clients value is less th
 else gnt_local= 4'b0;
 end
 
+assign  req_i_masked = req_i & ~m;
 assign gnt_o= gnt_local &  ~(gnt_local-1); //// this calculates the Right Most 1 bit
-	assign pos = $clog2(gnt_o); ///// This will calculate the position of the Last Granted request in the array
+assign pos = $clog2(gnt_o); ///// This will calculate the position of the Last Granted request in the array
 	
 endmodule
